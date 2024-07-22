1ba84a552f4f0a553a4c6d30e93b5973e1ccd5ca
Patihan Prasongphol <105841902+NinePTH@users.noreply.github.com>
2024-07-22 19:10:48 +0700

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -7,16 +7,16 @@ time.sleep(1)
 f = open("รายชื่อ.txt", encoding="utf-8-sig")
 
 print(pyautogui.position())
-
-for word in f:
-    time.sleep(1)
-    pyautogui.click(x=1156, y=271) #Duplicate ป้ายชื่อ
-    time.sleep(1)
-
-    pyautogui.doubleClick(x=996, y=736) #คลิกแก้ชื่อ
-    time.sleep(1)
-    pyperclip.copy(word)
-    pyautogui.typewrite("P'")
-    time.sleep(1)
-    pyautogui.hotkey('ctrl', 'v')
-    pyautogui.press('backspace')
+def automate():
+    for word in f:
+        time.sleep(1)
+        pyautogui.click(x=1156, y=271) #Duplicate ป้ายชื่อ
+        time.sleep(1)
+    
+        pyautogui.doubleClick(x=996, y=736) #คลิกแก้ชื่อ
+        time.sleep(1)
+        pyperclip.copy(word)
+        pyautogui.typewrite("P'")
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press('backspace')
